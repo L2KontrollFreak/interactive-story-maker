e7574fb29b9f31b967b65ace76ddb9b4e1769452
L2KontrollFreak <damarble2005@gmail.com>
2022-03-11 17:11:16 -0800

    add changes to css,html js,

--- story.js
+++ story.js
@@ -1,1 +1,5 @@
-var story
+window.onload = function(){
+    var story = {
+        opener: {
+            text:" Your in the world of Max Steel. You are Max McGraph who moved in to a new town because his mom new jobs in DrazTech. Max goes to a new school called Jolly Time highschool",
+            Option:[["home", "got to School"]], [["leave school", "Leave The Game"]], [["home", "Quit"]],      
