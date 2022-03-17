1a551161f0f3dc7a41044505424fc15b5be5be6c
L2KontrollFreak <damarble2005@gmail.com>
2022-03-16 19:11:22 -0700

    fix html and js

---
+++ story.js
@@ -0,0 +1,15 @@
+var story = 0;//the story is undefined
+var form = document.getElementById('MaxSteel');//elements with matching ids to spefic task
+var submit = document.getElementById('continueButton');
+var reset = document.getElementById('resetButton');
+var answer = '';
+
+ var story_begins = {
+     "start": {
+      "question":"You're in the world of Max Steel. You are Max McGraph who moved in to a new town because his mom new jobs in DrazTech. Max goes to a new school called Jolly Time highschool and you figure out that Max isn't your ordinary teenager. Max meets his uncle and finds out the truth about his superpowers. He has to prove he is worthy and responsibile in order to prove he is a good hero to society. It's up to you...",
+        "answers":{
+            "a": ""
+
+        }
+     }
+ }
