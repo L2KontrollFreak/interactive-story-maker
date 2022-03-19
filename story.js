dc522bfb875a59c2f41e2c23f8716a9d4a76bda3
L2KontrollFreak <damarble2005@gmail.com>
2022-03-18 21:14:12 -0700

    fix tons of stuff in js

--- story.js
+++ story.js
@@ -8,61 +8,85 @@ var answer = '';
 
 var story_tell = {
     "start": {
-        "dialogue": " This is a story test",
+        "dialogue": "max goes to a new town",
         "answers": {
-            "a": "okay",
-            "b": "not okay",
-            "c": "fudge",
+            "a": "yes:put stuff away and helps his mom put stuff away",
+            "b": "Neutral: doesn't finish cleaning up",
+            "c": "no:don't put stuff away in his room and Be lazy",
+
         }
     },
     //test
     "1_a": {
-        "dialogue": "this story test is amazing",
+        "dialogue": "Goes to school",
         "answers": {
-            "a": " this isn't amazing",
-            "b": "it's cool",
+            "a": " Max see new people and makes newfriends",
+            "b": "No: Max chooses to be alone",
             "c": "not bad at all",
         }
     },
-    "2_a":{
-      "dialogue": "do you like eating tacos",
-      "answers":{
-        "a": "yes",
-        "b": "no",
-        "C": "meh"
-      }
+    "2_a": {
+        "dialogue": "max see someone getting bully <br> Meet a guy name kirb",
+        "answers": {
+            "a": "Yes: stop the bullying and help the kid out <br> max walk him from school to his home ",
+            "b": "No: leaves it's alone and later regrets <br> he checks the guy to see if he is okay but later sees he is bruised <br> Ask the guy is okay and what is his name?",
+            "c": "netural: just stand there do nothing",
+        }
+
+    },
+
+    "3_a": {
+        "dialogue": "goe to see his uncle but something unexpected happen <br> explode this unknown energy in a abandoned building  or hold it in as long as he could ",
+        "answers": {
+            "a": "Yes:choose to explode in a abandoned building <br> and pass out from the explosion <br> wakes up",
+            "b": "No:max choose to hold it in as long as he could <br> then he's pass out <br> wake up"
+        }
+    },
+
+    "4_a": {
+        "dialogue": "Max meets his uncle named Ferris in a secret building <br> Max: hey unc where am I? <br> we are in N-tech inc  max?",
+        "answers":{
+            "a": "next"
+        }
+    },
+    "5_a":{
+        "dialogue": "should max follow unc ferris?",
+        "answers":{
+         "a": "yes and follow him to know what's happening to him",        
+         "b": "no and wants to go home <br> goes home <br> talk to his mom about it <br> mom finds out  about max powers then talks to her brother ferris <br> tells max the orgin of his powers.<br> max freaks out abour his orgin <br>thats his dad is a alien from a anothor planet <br> doesn't feel like he belongs <br> max moves away to try to find out the truth <br>  max learns to accept who he is <br> max stays as a normal kid and never became a hero <br> max never fought badguys but has steel to keep his T.U.R.B.O. Energy in check <br> he's graudates from HighSchool and has a girlfriend later in his life "
+        }    
 
     }
 };
 // continue link
-submit.addEventListener('mouseup', function(){ //when a button on a pointing device is released while the pointer is located inside it
+submit.addEventListener('mouseup', function () { //when a button on a pointing device is released while the pointer is located inside it
     answer = form.querySelectorAll('input[type=radio]:checked')[0].value; //a list of the document's elements that match the specified group of selectors.
-    if(answer) {
-      story++;//increment or add 1 to
-      populateForm(story + '_' + answer);//if a radio is checked populate our form with the answer
-      console.log("Story time!"); // Console log to make sure things are working
+    if (answer) {
+        story++;//increment or add 1 to
+        populateForm(story + '_' + answer);//if a radio is checked populate our form with the answer
+        console.log("Story time!"); // Console log to make sure things are working
     }
-  });
-  
-  // Reset button
-  function resetForm(){
-      document.getElementById("restartButton").reset();
-  }
-  
-  // Generate answers from story
-  function populateForm(story) {
+});
+
+// Reset button
+function resetForm() {
+    document.getElementById("restartButton").reset();
+}
+
+// Generate answers from story
+function populateForm(story) {
     var current_story = story_tell[story];//take values from story_telling story
     var text = '';
-  
-    for(var prop in current_story['answers']) {
-      if(current_story['answers'].hasOwnProperty(prop)) { //method returns a boolean (true or false) indicating whether the object has the specified property as its own property
-        text += '<label><input type="radio" name="answer" value="a" "b" "c" "a" "b" "c"' + prop + '"/><span>' + current_story['answers'][prop] + '</span></label>';// adding answers to the story
-      }
+
+    for (var prop in current_story['answers']) {
+        if (current_story['answers'].hasOwnProperty(prop)) { //method returns a boolean (true or false) indicating whether the object has the specified property as its own property
+            text += '<label><input type="radio" name="answer" value="a" "b" "c" "a" "b" "c"' + prop + '"/><span>' + current_story['answers'][prop] + '</span></label>';// adding answers to the story
+        }
     }
-  
+
     form.querySelector('p').innerHTML = current_story.dialogue;//write questions to the p tag in the HTML
     form.querySelector('fieldset').innerHTML = text;//write answers to the fieldset 
-  }
-  
-  populateForm('start');//set the form at the beginning
+}
+
+populateForm('start');//set the form at the beginning
 
