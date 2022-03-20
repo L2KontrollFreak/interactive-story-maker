4cfb785d74dbc22495e900c3d2915fee698b06b9
L2KontrollFreak <damarble2005@gmail.com>
2022-03-20 13:03:11 -0700

    add dialogue to js

--- story.js
+++ story.js
@@ -8,6 +8,10 @@ var answer = '';
 
 var story_tell = {
     "start": {
+        "dialogue": "You're in the world of Max Steel. You are Max McGraph who moved in to a new town because his mom new jobs in DrazTech. Max goes to a new school called Jolly Time highschool and you figure out that Max isn't your ordinary teenager. Max meets his uncle and finds out the truth about his superpowers. He has to prove he is worthy and responsibile in order to prove he is a good hero to society. It's up to you...",
+        "answers":{
+            "a": "next",
+        },
         "dialogue": "max goes to a new town",
         "answers": {
             "a": "yes:put stuff away and helps his mom put stuff away",
